26c24bb4d0ecc04e201b64a4782b3c3d3e123a64
Abhishek <abhishek.kumar@ucertify.com>
2020-05-13 14:09:59 +0530

    Updates

---
+++ precache-manifest.cd2c13a05f5b9683e40e20a5b2a41ffb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fef8b46f960aad699db54e28466a7f",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "90ac12f45c8b13d3e70f",
+    "url": "/covid-19/static/css/main.23b1fc77.chunk.css"
+  },
+  {
+    "revision": "49110a5b13699bf99f3a",
+    "url": "/covid-19/static/js/2.c91b3bfc.chunk.js"
+  },
+  {
+    "revision": "9f9171a5d352058c74e3ad3d31ccb652",
+    "url": "/covid-19/static/js/2.c91b3bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ac12f45c8b13d3e70f",
+    "url": "/covid-19/static/js/main.2d25f6c2.chunk.js"
+  },
+  {
+    "revision": "f0890fbbe69b862ac287",
+    "url": "/covid-19/static/js/runtime-main.91855f0b.js"
+  },
+  {
+    "revision": "f71bdafa92fefec17b7ae69adda01b76",
+    "url": "/covid-19/static/media/bg.f71bdafa.gif"
+  },
+  {
+    "revision": "662041f6bf3366494ea546ad92dec444",
+    "url": "/covid-19/static/media/logo.662041f6.png"
+  }
+]);
