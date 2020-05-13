81291b5050edc176aee4ce0b1e5fcdace0c476a4
Abhishek <abhishek.kumar@ucertify.com>
2020-05-13 14:17:53 +0530

    typo fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import {Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import {fetchData} from './api';
+import Logo from './image/logo.png'
+class App extends Component {
+    state = {
+        data :{},
+        country:"",
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({data:fetchedData});
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({
+            data:fetchedData,
+            country:country
+        });
+    }
+
+    render() {
+        const {data, country} = this.state;
+        return (
+            <div className={styles.container}>
+                <div className={styles.container}>
+                    <img className={styles.logospin} src={Logo} alt="COVID-19" />
+                    <p className={styles.logofont}>
+                        <span className={styles.c2}>Online </span>
+                        <span className={styles.c1}>COVID-19 </span>
+                        <span className={styles.c3}>Record </span>
+                        <span className={styles.c4}>Tracker</span>
+                    </p>
+                </div>
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data}  country={country} />
+            </div>
+        )
+    }
 }
 
 export default App;
