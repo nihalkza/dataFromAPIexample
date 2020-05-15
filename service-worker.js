f888dab5a0e549946dca6bcb3c9e6f0ce3b97199
Abhishek <abhishek.kumar@ucertify.com>
2020-05-15 19:50:33 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/new/precache-manifest.08a15c8853669442760e7dee53c17327.js"
+  "/new/precache-manifest.0fa5f4a7e78a90ff49c4713ef86427f6.js"
 );
 
 self.addEventListener('message', (event) => {
